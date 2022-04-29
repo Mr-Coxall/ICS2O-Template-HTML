49464a79bab2dd396374e4cdb17f9adfb1f95cb9
Patrick Coxall <mr.coxall@mths.ca>
2022-04-29 13:08:20 -0400

    linting fix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+/**
+ * This function is fired when the "Click" button is clicked.
+ */
 function myButtonClicked() {
   // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
